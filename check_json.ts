d2735a3a8262c9788a3750d5f966b5f06e83b2d8
mfaustinee <mfaustinee@gmail.com>
2026-03-18 18:32:38 +0300

    deleted unnecessary files,

--- check_json.ts
+++
@@ -1,9 +0,0 @@
-
-import fs from 'fs';
-try {
-  const data = fs.readFileSync('./data/agreements.json', 'utf-8');
-  const json = JSON.parse(data);
-  console.log('JSON is valid. Array length:', json.length);
-} catch (e) {
-  console.error('JSON is invalid:', e.message);
-}
--- get_size.ts
+++
@@ -1,4 +0,0 @@
-
-import fs from 'fs';
-const stats = fs.statSync('./data/agreements.json');
-console.log('Size:', stats.size, 'bytes');
